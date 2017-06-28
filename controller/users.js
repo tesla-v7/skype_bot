1f821c14e48791590c48eeb186f87d3c55391c8c
Vidanov V.V <tesla.v3@yandex.ru>
2017-06-28 16:24:37 +0600

    модуль работы с пользователями

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,33 @@
 'use strict';
+var models = require('../models/index');
 
 module.exports = {
     saveUser: function (data) {
-        return '';
+        let name = data.text.replace(/\s{2,}/g, ' ').split(/\s/).slice(2).join(' ');
+        console.log('-', name);
+        if(name.length < 3){
+            return `Пользователь ${data.user.name} слишком короткое имя '${name}'`;
+        }
+        return models.Users.findOrCreate({
+            where: {
+                user_id: data.user.id
+            },
+            defaults: {
+                user_name: data.user.name,
+                user_doc_name: name,
+            }
+        }).spread((user, created) => {
+            if(!created){
+                user.user_doc_name = name;
+                return user.save();
+            }
+            return;
+        })
+        .then(() =>{
+            return `Пользователь ${data.user.name} сопоставлне с именем '${name}'`;
+        })
+        .catch((e) => {
+            return `Ошибка сохранения имени для ${data.user.name}`;
+        });
     }
 };
