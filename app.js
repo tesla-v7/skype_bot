a1236d7addab32fd84380ac46d1c667ffcd66519
Vidanov V.V <tesla.v3@yandex.ru>
2017-07-20 14:02:47 +0600

    добавил заполнение табеля по расписанию добавил ответ в общий чат при персональном в обращении

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var config    = require(__dirname + '/config/config.json')[env || 'development']
 var restify   = require('restify');
 var builder   = require('botbuilder');
 
+var moment    = require('moment');
+var schedule  = require('node-schedule');
+
 var router    = require('./lib/router');
 var loger     = require('./controller/loger');
 var users     = require('./controller/users');
-var help      = require('./controller/help');
-var sync      = require('./controller/syncGoogleSheet');
+var util      = require('./controller/util');
+// var sync      = require('./controller/syncGoogleSheet');
 var task      = require('./controller/task');
-// var googleDoc = require('./lib/googleDoc');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -26,89 +28,49 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// googleDoc.setAuth(config.auth.google);
-sync.setAuth(config.auth.google);
-sync.setSheetParams(config.google_sheet);
-// sync.setSheetParams('test');
+task.setAuth(config.auth.google);
+task.setSheetParams(config.google_sheet);
 
-router.add(['in', 'out', 'пришел', 'ушел'], loger.saveLog);
-router.add('ping', help.ping);
-router.add('help', help.help);
-router.add('default', help.default);
-router.add('sync', sync.syncAll.bind(sync));
-router.add(['i', 'я'], users.saveUser);
-// router.add('test', task.createTask.bind(task));
-router.add('test', task.sevaTaskGooglDoc.bind(task));
+let msg = new builder.Message().address(config.skype.address);
 
-var bot = new builder.UniversalBot(connector, function (session) {
-    // session.sendTyping();
-    // console.log(session);
-    console.log(session.message);
-    Promise.resolve(router.go(session.message)).
-    then((resylt) => {
-        // if(Array.isArray(resylt)){
-        //     resylt.map(function (item) {
-        //         session.send(item);
-        //     })        //     return;
-        // }
-        session.send(resylt);
-    }).
-    catch((error) => {
-        session.send('Возникла ошибка');
+router.add(['in', 'out', 'пришел', 'ушел'], function (command, skypeData) {
+    let saveLog = loger.saveLog.bind(loger);
+    let chatId = config.skype.address.conversation.id;
+    let senderId = skypeData.address.conversation.id;
+    return Promise.resolve(saveLog(command, skypeData)).
+    then((text) =>{
+        if(chatId != senderId){
+            msg.text(`${skypeData.user.name} ${moment(skypeData.timestamp).format("YYYY.MM.DD HH:mm")} \r\n\r\n ${skypeData.text}`);
+            // msg.textLocale('en-US');
+            bot.send(msg);
+        }
+        return text;
+    });
+});
+router.add('ping', util.ping);
+router.add('help', util.help);
+router.add('default', util.default);
+// router.add('sync', sync.syncAll.bind(sync));
+router.add(['i', 'я'], users.run.bind(users));
+// router.add('test', task.saveTaskInGooglDoc.bind(task));
+let scheduleGoogle = schedule.scheduleJob(config.cron, function () {
+    let createTask = task.createTask.bind(task);
+    let saveTaskInGooglDoc = task.saveTaskInGooglDoc.bind(task);
+    Promise.resolve(createTask()).
+    then(() =>{
+        saveTaskInGooglDoc();
     });
 });
 
-var express = require('express');
-var app = express();
-
-
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-})
-
-var path = require('path');
-
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/user', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+var bot = new builder.UniversalBot(connector, function (session) {
+    console.log(session.message);
+    console.log(JSON.stringify(session.message.address));
+    Promise.resolve(router.go(session.message)).
+    then((resylt) => {
+        session.send(resylt);
+    }).
+    catch((error) => {
+        console.log(error);
+        session.send('Возникла ошибка');
+    });
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-module.exports = app;
